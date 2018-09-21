95da00fa0a630ec7755321b4c6446c07fb1097a9
psylogic <nemli.othmane@gmail.com>
2018-09-21 16:39:05 +0000

    remove notification from topbar

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -5,7 +5,7 @@
     <div class="navbar-collapse collapse">
         <ul class="navbar-nav ml-auto">
             @auth
-            <li class="nav-item"><a href="#" class="nav-link"><i class="fa fa-bell"></i> 3</a></li>
+            {{-- <li class="nav-item"><a href="#" class="nav-link"><i class="fa fa-bell"></i> 3</a></li> --}}
             <li class="nav-item dropdown">
                 <a href="#" id="dd_user" class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ Auth::user()->name ?? 'none' }}</a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd_user">
